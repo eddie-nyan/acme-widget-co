09e1b0650af08b726bda60abeaf767db8f001c07
Nyan Lin Htut <nyanlinhtut925@gmail.com>
2025-10-28 20:33:49 +0700

    Add README and Product class

---
+++ lib/product.rb
@@ -0,0 +1,10 @@
+# Represents a product in the catalogue
+class Product
+  attr_reader :code, :name, :price
+
+  def initialize(code:, name:, price:)
+    @code = code
+    @name = name
+    @price = price
+  end
+end
